bf13cccbddd7db965abd64e63cf883cc1047fda3
Ronan Cassidy <36006416+S00177754@users.noreply.github.com>
2020-04-28 16:52:08 +0100

    Player Movement Script #2 - Camera Refactor
    -Third Person Camera refactor to work similar to industry standards
    -Added Third person camera collision handler to stop camera clipping into walls
    -Refactor of Player Movement
    -Addition of player controller and player settings

---
+++ Assets/Ronan/Scripts/Player Mechanics/PlayerController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public PlayerSettings Settings;
+}
+
+[Serializable]
+public class PlayerSettings
+{
+    [Header("Camera Settings")]
+    public bool InvertYAxis = true;
+    public float CameraSensitivity = 2f;
+}
--- Assets/Ronan/Scripts/Player Mechanics/PlayerMovement.cs
+++ Assets/Ronan/Scripts/Player Mechanics/PlayerMovement.cs
@@ -1,64 +1,91 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions.Must;
 using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(CharacterController))]
 public class PlayerMovement : MonoBehaviour
 {
+    // ************** Public Variables **************
     [Header("Character States")]
     public bool FreezeMovement = false;
     public bool IsGrounded = true;
     public bool IsCrouching = false;
+    public bool IsClimbing = false;
+    
+    public bool IsJumping = false;
+    [HideInInspector]
+    public bool IsMoving = false;
 
     [Header("Movement Speeds")]
     public float WalkSpeed = 5f;
     public float SprintSpeed = 7.5f;
     public float CrouchSpeed = 2f;
     public float ClimbSpeed = 4.5f;
+    [HideInInspector]
     public float FallSpeed = -9.81f;
 
     [Header("Action Variables")]
     public float JumpHeight = 2f;
     public float CrouchHeight = 1f;
 
+    [Header("Camera")]
+    public Transform CameraParent;
+    public float LookYLimit = 60f;
+
     [Header("")]
+    public Transform PlayerModel;
     public Transform bottomOfPlayer;
     public LayerMask GroundLayer;
 
+    // ************** Private Variables **************
+    private PlayerController Controller;
+
     //Private Variables
     private CharacterController Character;
-    private const float GroundCheckRadius = 0.1f;
-
     private Vector2 input_Move;
     private Vector3 velocity;
-    private bool IsMoving = false;
 
     //Used for speed calulations and set with the speed variables above
     private float movementSpeed = 5f;
-    private float xRotation = 0f;
+    private float slideFiction = 0.3f;
+    private Vector3 hitNormal;
+    private float forwardMultiplier = 1;
+    private Vector3 forwardDirection;
 
 
+    //Camera Rotation 
+    private Vector2 rotation = Vector2.zero;
+    private Vector3 moveDirection = Vector3.zero;
+
+    // ************** Monobehaviour Methods **************
     void Start()
     {
+        Controller = GetComponent<PlayerController>();
         Character = GetComponent<CharacterController>();
+        rotation.y = transform.eulerAngles.y;
     }
 
     void Update()
     {
-        IsGrounded = TouchingGround();
 
-        if(IsGrounded && velocity.y < 0)
-        {
-            velocity.y = -2f;
-        }
+        ResetVelocity();
 
         Move(input_Move);
-        Gravity();
+
+        IsGrounded = TouchingGround();
+        
+        Gravity();  
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        IsGrounded = true;
     }
 
 
-
+    // ************** Movement Methods **************
 
     /// <summary>
     /// Movement calculations for gameobject.
@@ -75,33 +102,65 @@ public class PlayerMovement : MonoBehaviour
                 return;
 
             var scaledMoveSpeed = movementSpeed * Time.deltaTime;
-            var move = Quaternion.Euler(0, transform.eulerAngles.y, 0) * new Vector3(direction.x, 0, direction.y);
 
-            Character.Move(move * scaledMoveSpeed);
+            Vector3 forward = transform.TransformDirection(Vector3.forward);
+            Vector3 right = transform.TransformDirection(Vector3.right);
+
+            float curSpeedX = scaledMoveSpeed * direction.y;
+            float curSpeedY = scaledMoveSpeed * direction.x;
+            moveDirection = (forward * curSpeedX) + (right * curSpeedY);
+
+            //Player model rotation so they are facing the correct position
+            PlayerModel.rotation = Quaternion.Slerp(PlayerModel.rotation, Quaternion.LookRotation(moveDirection), 0.15F);
+            
+            //Moves position of Character
+            Character.Move(moveDirection);
         }
     }
 
+    private void Look(Vector2 direction)
+    {
+        if (!FreezeMovement)
+        {
 
+            rotation.y += direction.x * Controller.Settings.CameraSensitivity;
+            if (Controller.Settings.InvertYAxis)
+            {
+                rotation.x += -direction.y * Controller.Settings.CameraSensitivity;
+            }
+            else
+            {
+                rotation.x += direction.y * Controller.Settings.CameraSensitivity;
+            }
+            rotation.x = Mathf.Clamp(rotation.x, -LookYLimit, LookYLimit);
+
+            CameraParent.localRotation = Quaternion.Euler(rotation.x, 0, 0);
+            transform.eulerAngles = new Vector2(0, rotation.y);
+        }
+    }
+
+    private void ResetVelocity()
+    {
+        if (IsGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+    }
 
     private void Gravity()
     {
+        if (velocity.y <= -2)
+        {
+            IsJumping = false;
+        }
+
         if (!IsGrounded)
         {
+
             velocity.y += 2f * FallSpeed * Time.deltaTime; // y = ((1/2)*g) * t^2
 
             Character.Move(velocity * Time.deltaTime); //T = time.deltaTime
         }
-    }
-
-    private void SlideCheck()
-    {
-        //Raycast Check
-        RaycastHit[] hits;
-        RaycastRing(bottomOfPlayer.position, Vector3.down, 0.2f, out hits, 0.15f, GroundLayer);
-
-        //Need to check if player centre is above the slope and in range
-
-        //if true then find angle of slope and apply movement force
     }
 
     private bool TouchingGround()
@@ -109,12 +168,14 @@ public class PlayerMovement : MonoBehaviour
         RaycastHit[] hits;
 
         //Need to refactor but raycasting around the character allows for properground check, by sinking slightly into collider of player, we avoid wall collision issues
-        if(RaycastRing(bottomOfPlayer.position,Vector3.down,0.2f,out hits,0.15f,GroundLayer))
+        if (RaycastRing(bottomOfPlayer.position, Vector3.down, 0.2f, out hits, 0.15f, GroundLayer))
         {
             return true;
         }
 
         return false;
+
+
     }
 
     private bool RaycastRing(Vector3 position,Vector3 direction,float radius,out RaycastHit[] hits, float MaxDistance, LayerMask layer)
@@ -132,6 +193,7 @@ public class PlayerMovement : MonoBehaviour
     }
 
 
+    // ************** Input Action Methods **************
 
     #region Input Actions
     public void OnMove(InputAction.CallbackContext context)
@@ -152,7 +214,7 @@ public class PlayerMovement : MonoBehaviour
 
     public void OnLook(InputAction.CallbackContext context)
     {
-
+        Look(context.ReadValue<Vector2>());
     }
 
 
@@ -166,6 +228,7 @@ public class PlayerMovement : MonoBehaviour
                     velocity.y = Mathf.Sqrt(JumpHeight * -2 * FallSpeed); //v = Square root of (h * -2 * g)
                     Character.Move(velocity * Time.deltaTime);
                     IsGrounded = false;
+                    IsJumping = true;
                 }
                 break;
 
@@ -206,6 +269,8 @@ public class PlayerMovement : MonoBehaviour
 
     #endregion
 
+
+    // ************** Debug **************
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.magenta;
