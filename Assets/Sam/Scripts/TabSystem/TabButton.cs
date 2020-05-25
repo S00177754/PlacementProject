3edec4f66e5b30630cd9c8e812f3161e4c19a2e1
Sam Colville <s00182360@mail.itsligo.ie>
2020-05-25 17:28:53 +0100

    Basic UI tab system implemented

---
+++ Assets/Sam/Scripts/TabSystem/TabButton.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class TabButton : MonoBehaviour, IPointerEnterHandler, IPointerClickHandler, IPointerExitHandler
+{
+    public TabGroup tabGroup;
+
+    public Text tabUIText;
+    public bool tabSelected;
+
+
+    public void OnPointerClick(PointerEventData eventData){
+        tabGroup.OnTabSelected(this);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData){
+        tabGroup.OnTabEnter(this);
+    }
+
+    public void OnPointerExit(PointerEventData eventData){
+        tabGroup.OnTabExit(this);
+    }
+
+    void Start()
+    {
+        tabUIText = GetComponent<Text>();
+        tabGroup.Subscribe(this);
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Sam/Scripts/TabSystem/TabGroup.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TabGroup : MonoBehaviour
+{
+    public List<TabButton> tabButtons;
+    public Color tabIdle;
+    public Color tabHover;
+    public Color tabActive;
+
+    public void Subscribe(TabButton tabButton){
+        if(tabButtons == null)
+            tabButtons = new List<TabButton>();
+
+        tabButtons.Add(tabButton);
+
+        // tabIdle = new Color(1,1,1,0.5f);
+        // tabHover = new Color(0.5f,0.5f,0.5f,0.75f);
+        // tabActive = new Color(0.9f, 0.2f,0.2f,0.75f);
+    }
+
+    public void OnTabEnter(TabButton tabButton){
+        tabButton.tabUIText.color = tabHover;
+    }
+
+    public void OnTabExit(TabButton tabButton){
+        tabButton.tabUIText.color = tabIdle;
+    }
+
+    public void OnTabSelected(TabButton tabButton){
+        if(!tabButton.tabSelected){
+            //if tab is not selected, chage to active color and switch to is active
+            tabButton.tabUIText.color = tabActive;
+            tabButton.tabSelected = !tabButton.tabSelected;
+        }
+        else{
+            //If tab is active, change to hover color
+            tabButton.tabUIText.color = tabHover;
+            tabButton.tabSelected = !tabButton.tabSelected;
+        }
+    }
+}
