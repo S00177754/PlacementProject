4990690298b71e3e482d0e06a6a2326f1daf3314
Ronan Cassidy <36006416+S00177754@users.noreply.github.com>
2020-06-27 12:34:48 +0100

    Auto Scroll Fixes

--- Assets/Ronan/Scripts/UI Systems/AutoScrollFocus.cs
+++ Assets/Ronan/Scripts/UI Systems/AutoScrollFocus.cs
@@ -40,14 +40,21 @@ public class AutoScrollFocus : MonoBehaviour
         //Calculations
         Vector3 selectedDifference = RectTransform.localPosition - SelectedContent.localPosition;
         float contentHeightDifference = (Content.rect.height - RectTransform.rect.height);
+        float contentWidthDifference = (Content.rect.width - RectTransform.rect.width);
 
         float selectedPosition = (Content.rect.height - selectedDifference.y);
         float currentScrollRectPosition = ScrollViewRect.normalizedPosition.y * contentHeightDifference;
         float above = currentScrollRectPosition - (SelectedContent.rect.height) + RectTransform.rect.height;
         float below = currentScrollRectPosition + (SelectedContent.rect.height);
 
+        float selectedHorizontalPosition = (Content.rect.width - selectedDifference.x);
+        float currentHorizontalScrollRectPosition = ScrollViewRect.normalizedPosition.x * contentWidthDifference;
+        float left = currentHorizontalScrollRectPosition - (SelectedContent.rect.width) + RectTransform.rect.width;
+        float right = currentHorizontalScrollRectPosition + (SelectedContent.rect.width);
+
         // check if selected is out of bounds
         float step = 0;
+        float stepX = 0;
 
         if (selectedPosition > above)
         {
@@ -57,8 +64,21 @@ public class AutoScrollFocus : MonoBehaviour
         {
             step = selectedPosition - below;
         }
+
+        //if (selectedHorizontalPosition > left)
+        //{
+        //    stepX = selectedHorizontalPosition + left;
+        //}
+        //else if (selectedHorizontalPosition < right)
+        //{
+        //    stepX = selectedHorizontalPosition + right;
+        //}
+
             float newY = currentScrollRectPosition + step;
+            float newX = currentHorizontalScrollRectPosition + stepX;
+
             float newNormalizedY = newY / contentHeightDifference;
+            //float newNormalizedX = newX / contentWidthDifference;
             ScrollViewRect.normalizedPosition =  new Vector2(0, newNormalizedY);
 
     }
