ae377a0de4486aa72a43835d33c88488e328996a
Sam <s00182360@mail.itsligo.ie>
2020-06-30 18:02:08 +0100

    Quests being read from Manager

--- Assets/Sam/Scripts/TabSystem/QuestSelection.cs
+++ Assets/Sam/Scripts/TabSystem/QuestSelection.cs
@@ -23,14 +23,26 @@ public class QuestSelection : TabGroup
     {
         //Load cuttent Quest list
         Manager = Resources.Load<QuestManager>("ScriptableObjects/QuestManager");
+
+        //Debugging
         if (Manager == null)
             Debug.Log("Quest Manager Null");
+        if (Manager.FoundMSQuests == null)
+            Debug.Log("FoundMSQuest is null");
+        else if (Manager.FoundMSQuests.Count == 0)
+            Debug.Log("FoundMSQuest not null count = 0");
+        else if (Manager.FoundMSQuests.Count > 0)
+            Debug.Log(string.Format("FoundMSQuests contains {0} elements", Manager.FoundMSQuests.Count));
+
         foreach (var item in Manager.FoundMSQuests)
         {
             GameObject next = Instantiate(ScrollContent); //Creates new Scrollview Content
             next.TryGetComponent<Text>(out QuestName);
             if (QuestName != null)
+            {
                 QuestName.text = item.Name;
+                Debug.Log(QuestName);
+            }
             Debug.Log("Quest added: " + item.Name);
         }
 
