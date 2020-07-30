042ef6cd774af52b36b85cb2a8ef54df28e4ea51
Ronan Cassidy <S00177754@mail.itsligo.ie>
2020-07-30 11:13:57 +0100

    Title Screen Work
    -Setup of buttons on title screen
    -Settings menu on title screen
    -Backdrop for title screen added

---
+++ Assets/Ronan/Scripts/TitleScreenScripts/TitleMenuController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleMenuController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void NewGame()
+    {
+        //TODO Create new save file and load world
+    }
+
+    public void LoadGame()
+    {
+        //TODO Show list of save files
+    }
+
+    public void Settings()
+    {
+        //TODO Show settings
+        //Can probably just grab them from the game scene
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+}
