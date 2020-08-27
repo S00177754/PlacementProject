3c7b477e8337f385a54ce26d022764e3d5f0424a
Ronan Cassidy <S00177754@mail.itsligo.ie>
2020-08-27 19:31:32 +0100

    Quest Work Part 3
    - Updated Quest Descriptions
    - Added more quest related item pickups to quest step spawner
    - Started work on visible quest location marker for location quest step

---
+++ Assets/Ronan/Scripts/QuestMarkers/QuestLocationMarker.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestLocationMarker : MonoBehaviour
+{
+    [Header("Marker Data")]
+    public Vector3 IconHeight;
+    public Sprite Icon;
+}
